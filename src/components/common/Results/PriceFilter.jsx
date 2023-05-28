03fa2e4515a36938dff74709a6c11b6eb8fc7c6c
YuukiNyan <yuukina0909@gmail.com>
2023-05-28 23:18:26 +0700

    update price filter slider

--- src/components/common/Results/PriceFilter.jsx
+++ src/components/common/Results/PriceFilter.jsx
@@ -13,33 +13,41 @@ import {
   BsCalendar2
 } from "react-icons/bs";
 import { Input, Slider } from "antd";
+import { useState } from "react";
 
 const PriceFilter = () => {
-  return (
-    <div className="results__filter__price-filter">
-		<div className="results__filter__price-filter__price-input">
-			<div className="results__filter__price-filter_price-input__header">
-				<div className="results__filter__price-filter__price-input__header__title">Giá</div>
-				<div className="results__filter__price-filter__price-input__header__description">
-					Giá phòng mỗi đêm
+	//Nhớ chỗ này mốt lấy dữ liệu
+	const [priceInput, setPriceInput] = useState([0, 3000000])
+	const handleSliderOnChange = (value) => {
+		setPriceInput(value);
+	}
+  	return (
+		<div className="results__filter__price-filter">
+			<div className="results__filter__price-filter__price-input">
+				<div className="results__filter__price-filter_price-input__header">
+					<div className="results__filter__price-filter__price-input__header__title">Giá</div>
+					<div className="results__filter__price-filter__price-input__header__description">
+						Giá phòng mỗi đêm
+					</div>
+				</div>
+				<div className="results__filter__price-filter__price-input__txb">
+					<Input id="minPrice" value={priceInput[0]} suffix="đ" className="price-filter__input"/>
+					<div className="results__filter__price-filter__line"></div>
+					<Input id="maxPrice" value={priceInput[1]} suffix="đ" className="results__filter__price-filter__input"/>
 				</div>
 			</div>
-			<div className="results__filter__price-filter__price-input__txb">
-				<Input value="0" suffix="đ" className="price-filter__input"/>
-				<div className="results__filter__price-filter__line"></div>
-				<Input value="3.000.000"suffix="đ" className="results__filter__price-filter__input"/>
-			</div>
+			<Slider
+				formatter={value => `${value}đ`}
+				className="results__filter__price-filter__slider"
+				range
+				min={0} 
+				max={3000000}
+				step={100000}
+				defaultValue={[0, 3000000]}
+				onChange={(value) => handleSliderOnChange(value)}
+			/>
 		</div>
-		<Slider
-			className="results__filter__price-filter__slider"
-			range
-			min={0} 
-			max={3000000}
-			step={100000}
-			defaultValue={[0, 3000000]}
-		/>
-	</div>
-  );
+ 	 );
 };
 
 export default PriceFilter;
