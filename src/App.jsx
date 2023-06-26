a05b5f93b659e2e1a38ead1ffee98d2788ed684f
YuukiNyan <yuukina0909@gmail.com>
2023-06-27 01:41:19 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,34 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { publicRoutes } from "./routes";
 import { MainLayout } from "./components/layout";
 import { Fragment } from "react";
+import AuthProvider from "./Context/AuthProvider";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        {publicRoutes.map((route, index) => {
-          const Page = route.element;
-          let Layout = MainLayout;
+      <AuthProvider>
+        <Routes>
+          {publicRoutes.map((route, index) => {
+            const Page = route.element;
+            let Layout = MainLayout;
 
-          if (route.layout) Layout = route.layout;
-          else if (route.layout === null) Layout = Fragment;
+            if (route.layout) Layout = route.layout;
+            else if (route.layout === null) Layout = Fragment;
 
-          return (
-            <Route
-              key={index}
-              path={route.path}
-              element={
-                <Layout>
-                  <Page />
-                </Layout>
-              }
-            ></Route>
-          );
-        })}
-      </Routes>
+            return (
+              <Route
+                key={index}
+                path={route.path}
+                element={
+                  <Layout>
+                    <Page />
+                  </Layout>
+                }
+              ></Route>
+            );
+          })}
+        </Routes>
+      </AuthProvider>
     </Router>
   );
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,12 @@
 import React from "react";
+import { auth } from "../firebase/config";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
 export const Login = () => {
-  return <div>Login</div>;
+
+  return (
+    <div>
+      Login
+    </div>
+  );
 };
