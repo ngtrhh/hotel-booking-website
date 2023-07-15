af6a248afa57360d1cb37d7c977cb423f2c43795
ngtrhh <91332224+ngtrhh@users.noreply.github.com>
2023-07-15 15:39:39 +0700

    fix style

--- src/components/common/BookingHistory/HorizontalCard.jsx
+++ src/components/common/BookingHistory/HorizontalCard.jsx
@@ -1,8 +1,14 @@
 import React, { useContext, useState } from "react";
 import Button from "../Button";
-import { BsStarFill, BsHeart, BsGeoAltFill } from "react-icons/bs";
+import { BsStarFill, BsHeart, BsGeoAltFill, BsHeartFill } from "react-icons/bs";
 import image from "../../../assets/images/ImageBanner.png";
-import { format, addDays, differenceInDays, formatDistance, differenceInMilliseconds } from "date-fns";
+import {
+  format,
+  addDays,
+  differenceInDays,
+  formatDistance,
+  differenceInMilliseconds,
+} from "date-fns";
 import { AppContext } from "../../../Context/AppProvider";
 import { useNavigate } from "react-router";
 
@@ -10,7 +16,7 @@ const state = ["passed", "coming", "canceled"];
 
 export const HorizontalCard = (props) => {
   const dataProvided = useContext(AppContext);
-  const {bookingHistoryData, setBookingHistoryData} = dataProvided;
+  const { bookingHistoryData, setBookingHistoryData } = dataProvided;
   const navigate = useNavigate();
   const data = props.DataToShow;
   // let state = parseFloat(formatDistance(
@@ -21,12 +27,11 @@ export const HorizontalCard = (props) => {
   //Nhập bookingHistoryData, setBookingHistoryData từ AppContent
 
   const HandleViewDetail = () => {
-    if(props.type === "favourite"){
-      navigate('/detail/' + data.accomId);
-    }
-    else{
+    if (props.type === "favourite") {
+      navigate("/detail/" + data.accomId);
+    } else {
       setBookingHistoryData(data);
-      navigate('/booking-history/detail');
+      navigate("/booking-history/detail");
     }
   };
 
@@ -42,10 +47,17 @@ export const HorizontalCard = (props) => {
           </div>
         </div>
         {props.type === "history" && props.state === state[0] && (
-          <div className="date">{'Đã đặt ngày: ' + data.orderDate.toDate().toLocaleDateString('vi-VN')}</div>
+          <div className="date">
+            {"Đã đặt ngày: " +
+              data.orderDate.toDate().toLocaleDateString("vi-VN")}
+          </div>
         )}
         {props.type === "history" && props.state === state[1] && (
-          <div className="remind">{'Còn ' + Math.ceil((data.recvDate.toDate() - Date.now()) / 86400000) + ' ngày nữa nhận phòng!'}</div>
+          <div className="remind">
+            {"Còn " +
+              Math.ceil((data.recvDate.toDate() - Date.now()) / 86400000) +
+              " ngày nữa nhận phòng!"}
+          </div>
         )}
         {props.type === "history" && props.state === state[2] && (
           <div className="date canceled">Đã hủy ngày 09/02/2023</div>
@@ -55,7 +67,11 @@ export const HorizontalCard = (props) => {
         <div className="wrapper">
           <div className="name">
             {data.accomsName || data.name}
-            <BsHeart size={40} />
+            {props.type === "favourite" ? (
+              <BsHeartFill size={40} />
+            ) : (
+              <BsHeart size={40} />
+            )}
           </div>
           <div className="address">
             <BsGeoAltFill size={18} />
@@ -63,14 +79,18 @@ export const HorizontalCard = (props) => {
           </div>
           <div className="rating">
             <div className="rating__score">{data.rating}</div>
-            <div className="rating__reviews">{data.ratingCount + ' lượt đánh giá'}</div>
+            <div className="rating__reviews">
+              {data.ratingCount + " lượt đánh giá"}
+            </div>
           </div>
           {props.type === "favourite" && (
             <>
-              <div className="flex-end old-price">{data.originalPrice + ' đ'}</div>
+              <div className="flex-end old-price">
+                {data.originalPrice + " đ"}
+              </div>
               <div className="flex-end">
                 <div className="new-price">
-                  Từ: <span>{data.price + ' đ'}</span>
+                  Từ: <span>{data.price + " đ"}</span>
                 </div>
                 <div className="sub">/đêm</div>
               </div>
@@ -84,11 +104,13 @@ export const HorizontalCard = (props) => {
                 <div className="wrapper__row__item__title">
                   Ngày nhận phòng:
                 </div>
-                <span>{data.recvDate.toDate().toLocaleDateString('vi-VN')}</span>
+                <span>
+                  {data.recvDate.toDate().toLocaleDateString("vi-VN")}
+                </span>
               </div>
               <div className="wrapper__row__item">
                 <div className="wrapper__row__item__title">Ngày trả phòng:</div>
-                <span>{data.endDate.toDate().toLocaleDateString('vi-VN')}</span>
+                <span>{data.endDate.toDate().toLocaleDateString("vi-VN")}</span>
               </div>
             </div>
             <div className="wrapper__row">
@@ -98,27 +120,28 @@ export const HorizontalCard = (props) => {
               </div>
               <div className="wrapper__row__item">
                 <div className="wrapper__row__item__title">Số người:</div>
-                <span>{
-            data.children > 0
-              ? `${data.guest} người - ${data.children} trẻ em - ${data.numOfRooms} phòng`
-              : `${data.guest} người - ${data.numOfRooms} phòng`
-          }</span>
+                <span>
+                  {data.children > 0
+                    ? `${data.guest} người - ${data.children} trẻ em - ${data.numOfRooms} phòng`
+                    : `${data.guest} người - ${data.numOfRooms} phòng`}
+                </span>
               </div>
             </div>
             <div className="wrapper__room">
-              <div className="wrapper__room__name">
-                {data.roomTypeName}
-              </div>
+              <div className="wrapper__room__name">{data.roomTypeName}</div>
               <span>{data.oneRoomPrice}</span>
             </div>
             <div className="wrapper__total">
-              Tổng cộng: <span>{data.orderPrice.toLocaleString('vi-VN') + ' đ'}</span>
+              Tổng cộng:{" "}
+              <span>{data.orderPrice.toLocaleString("vi-VN") + " đ"}</span>
             </div>
           </div>
         )}
 
         <div className="button-wrapper">
-          <Button onClick={HandleViewDetail} className="outline">Xem chi tiết</Button>
+          <Button onClick={HandleViewDetail} className="outline">
+            Xem chi tiết
+          </Button>
         </div>
       </div>
     </div>
