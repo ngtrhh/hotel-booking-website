54198bd423d87eb1bb9227c32cacc5993ba50537
ngtrhh <91332224+ngtrhh@users.noreply.github.com>
2023-07-15 19:29:21 +0700

    push

--- src/pages/Favourite.jsx
+++ src/pages/Favourite.jsx
@@ -1,35 +1,39 @@
 import React, { useContext, useState, useEffect } from "react";
 import { HorizontalCard } from "../components/common/BookingHistory/HorizontalCard";
 import { AppContext } from "../Context/AppProvider";
+import Helmet from "../components/common/Helmet";
 
 export const Favourite = () => {
   const data = useContext(AppContext);
-  const {accoms, orders, user} = data;
+  const { accoms, orders, user } = data;
   const [dataToShow, setDataToShow] = useState([]);
-  useEffect(() =>{
+  useEffect(() => {
     // const newDataToShow = orders.map((order) => {
     //   const correspondingAccom = accoms.find((accom) => accom.accomId === order.accomId && accom);
     //   if (correspondingAccom) {
-    //     return { ...order, ...correspondingAccom, 
+    //     return { ...order, ...correspondingAccom,
     //       accomsName: correspondingAccom.name};
     //   }
     //   return order;
     // });
-    const newDataToShow = []
+    const newDataToShow = [];
     let isNew = true;
-    accoms.map((accom) => {
-      if(user.lovedRoomsId.includes(accom.accomId)){
-        newDataToShow.map((accomToShow) =>{
-          if(accomToShow.accomId === accom.accomId){
-            isNew = false;
+
+    if (user) {
+      accoms.map((accom) => {
+        if (user.lovedRoomsId.includes(accom.accomId)) {
+          newDataToShow.map((accomToShow) => {
+            if (accomToShow.accomId === accom.accomId) {
+              isNew = false;
+            }
+          });
+          if (isNew) {
+            newDataToShow.push(accom);
           }
-        });
-        if(isNew){
-          newDataToShow.push(accom);
         }
-      }
-      isNew = true
-    });
+        isNew = true;
+      });
+    }
     console.log(newDataToShow);
     setDataToShow(newDataToShow);
   }, [accoms, orders, user]);
@@ -40,18 +44,36 @@ export const Favourite = () => {
   //   });
   //   setSortOption(updatedSortOption);
   // };
-  useEffect(() =>{
+  useEffect(() => {
     console.log(dataToShow);
   }, [dataToShow]);
 
   return (
-    <div className="favourite">
-      <div className="title">Yêu thích của tôi</div>
-      <div className="content">
-        {dataToShow.map((bookingHistory) => {
-          return (<HorizontalCard DataToShow={bookingHistory} type="favourite" state="passed" />)
-        })}
+    <Helmet title="Danh sách yêu thích">
+      <div className="favourite">
+        <div className="title">Yêu thích của tôi</div>
+        <div className="content">
+          {user ? (
+            dataToShow.map((bookingHistory) => {
+              return (
+                <HorizontalCard
+                  DataToShow={bookingHistory}
+                  type="favourite"
+                  state="passed"
+                />
+              );
+            })
+          ) : (
+            <div className="unlogged">
+              Đăng nhập để lưu các chỗ nghỉ mà bạn quan tâm!
+              <img
+                src={require("../assets/images/EmptyFavourite.png")}
+                style={{ width: "50%" }}
+              />
+            </div>
+          )}
+        </div>
       </div>
-    </div>
+    </Helmet>
   );
 };
